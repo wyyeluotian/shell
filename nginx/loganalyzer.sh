76061975b5214bc993a375c676ed1a17365d37ee
wyyeluotian <956663609@qq.com>
2016-12-07 13:23:42 +0800

    Update loganalyzer.sh

--- nginx/loganalyzer.sh
+++ nginx/loganalyzer.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #descrption  analysis the web accesslog
+#find top 10 ip and access count
 #date  2016-12-7
 
 #firstly,you should merge the accesslog.secondly,use awk deal with the text
 #first scrpit
-#!/bin/bash
-awk 'if($9==200 & $7 ~/^\/2022/.*.html$){sort[$]+=1;for(i in sort) print i,sort[i]}' /tmp/test.log
+awk 'if($9==200 & $7 ~/^\/2022/.*.html$){sort[$7]+=1;for(i in sort) print i,sort[i]}' /tmp/test.log
+
